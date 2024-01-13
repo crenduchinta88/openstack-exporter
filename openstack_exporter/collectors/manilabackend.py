4e98902f13dd3b0a5fc245509c2d3e51fbe33e8f
crenduchinta88 <77858438+crenduchinta88@users.noreply.github.com>
2024-01-13 14:53:31 -0500

    Update manilabackend.py

--- openstack_exporter/collectors/manilabackend.py
+++ openstack_exporter/collectors/manilabackend.py
@@ -20,14 +20,14 @@ class ManilaBackendCollector(BaseCollector.BaseCollector):
         os_username = self.config['username']
         os_password = self.config['password']
         os_project_name = self.config['project_name']
-        #os_endpoint = "https://share-3.qa-de-1.cloud.sap/v2"
+        os_endpoint = "https://share-3.qa-de-1.cloud.sap/v2"
         api_version = '2.65'  # Adjust the API version as needed
         
         client_args = dict(
             region_name=self.region,
             service_type="sharev2",
             service_name="manilav2",
-            os_endpoint='',
+            os_endpoint='os_endpoint',
             endpoint_type="publicURL",
             #insecure=False,
             cacert=None,
@@ -36,6 +36,8 @@ class ManilaBackendCollector(BaseCollector.BaseCollector):
             session=self.client.session,
         )
 
+        logger.debug(f"Service catalog: {self.client.service_catalog}")
+        
         return manila.Client(
             api_version,
             os_username,
@@ -79,7 +81,10 @@ class ManilaBackendCollector(BaseCollector.BaseCollector):
         return GaugeMetricFamily(name, description, labels=labels, value=value)
 
     def collect(self):
-        endpoint_url = "https://share-3.qa-de-1.cloud.sap/v2/scheduler-stats/pools/detail"
+        endpoint_url = "/scheduler-stats/pools/detail"
+        
+        logger.debug(f"Making API request to endpoint: {endpoint_url}") 
+        
         try:
             response = self.manila_client.session.get(endpoint_url)
             if response.status_code == 200:
